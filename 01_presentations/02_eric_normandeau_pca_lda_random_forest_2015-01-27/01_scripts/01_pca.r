7fb0c7c7443fd9163bbbebde5f6b39bb78d22310
Eric Normandeau <eric.normandeau.qc@gmail.com>
2015-01-27 10:17:15 -0500

    Added SVD example

--- 01_presentations/02_eric_normandeau_pca_lda_random_forest_2015-01-27/01_scripts/01_pca.r
+++ 01_presentations/02_eric_normandeau_pca_lda_random_forest_2015-01-27/01_scripts/01_pca.r
@@ -1,8 +1,32 @@
 # Clean environement
 rm(list=ls())
 
+
+##### About SVD
+# Creating fake dataset
+set.seed(123)
+x = rnorm(100, 5, 3)
+y = x/2 + rnorm(100, 0, 1)
+data = data.frame(x, y)
+plot(data, pch=16, cex=1.5, col="#00000088", xlim=c(0, 12), ylim=c(0, 12))
+
+##### Using the FactoMineR PCA function
+# install.packages("FactoMineR")
+library(FactoMineR)
+pca = PCA(data)
+
+# Plot importance of different axes
+barplot(pca$eig[,1])
+
+# Explore which factors play a major role in PC1 and PC2
+pca$var$coord
+
+################################################################################
+
+##### Using the mtcars dataset
 # Load data
 # good datasets: mtcars, swiss, airquality
+rm(list=ls())
 data = mtcars
 
 # Plot data to have a look
